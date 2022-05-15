3b9faa631e2d04d2827ebea870dd3fa0ee7bb0ab
onurkanit <onrkanit3@gmail.com>
2022-05-15 18:04:49 +0300

    [Changed] Player Prefab

--- HeadBall/Assets/Main/Scripts/GamePlay/Ball/Ball.cs
+++ HeadBall/Assets/Main/Scripts/GamePlay/Ball/Ball.cs
@@ -9,7 +9,6 @@ public class Ball : MonoBehaviour
 
     public void Shoot(Vector3 dir)
     {
-        Debug.Log("shoot");
-        rb.AddForce((dir + Vector3.up * 2f) * shootPower,ForceMode.Impulse);
+        rb.AddForce((dir + Vector3.up) * shootPower,ForceMode.Impulse);
     }
 }
